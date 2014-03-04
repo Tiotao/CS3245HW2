1ffa7150acaa12b59c402a744b1e7d0657f5e8c2
Minqi Chen <cnchenminqi@gmail.com>
2014-03-04 19:07:36 +0800

    add percentage completed when indexing

--- index.py
+++ index.py
@@ -2,6 +2,7 @@
 from nltk.tokenize import word_tokenize, sent_tokenize
 from nltk.stem.porter import *
 from os import walk
+import sys
 
 PATH = "D:\\training\\"
 DICT_DIR = 'D:\\dictionary.txt'
@@ -11,13 +12,15 @@ from config import *
 # TODO: use args
 
 def index():
-	
+	print 'start indexing'	
 	masterDict = {}
 
 	filenames = []
 	for (root, dirs, files) in walk(PATH):
 		filenames.extend(files)
 	
+	total = len(filenames)
+	curr = 0
 	for filename in filenames:
 		words = tokenize_files(filename)
 		tokens = stemming_words(words)
@@ -28,10 +31,13 @@ def index():
 					masterDict[key].append(int(filename))
 			else:
 				masterDict[key] = [int(filename)]
+		curr += 1
+		sys.stdout.write("\r" + ("%.2f" % (100.0 * curr / total)) + '%')
+		sys.stdout.flush()
 
 	f = open(DICT_DIR, 'w')
 	f.write(str(masterDict))
-	print 'index finished'
+	print "\nindex finished"
 	f.close()
 
 
